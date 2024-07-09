e1df870c1c3890217b6fd19c12eebde90771bd24
fowzia-fariha <farihashaj75@gmail.com>
2024-07-09 10:45:44 +0600

    Wooden Frame for MenuScreen

--- core/src/com/junglerush/MainMenuScreen.java
+++ core/src/com/junglerush/MainMenuScreen.java
@@ -3,6 +3,7 @@ package com.junglerush;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.math.MathUtils;
@@ -11,11 +12,13 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MainMenuScreen implements Screen {
 
     private final JungleRush game;
+    private final Texture logoImage;
     private float opacityControl = 0.3f;
     private boolean increase = true;
     public MainMenuScreen(JungleRush game)
     {
         this.game = game;
+        this.logoImage = new Texture(Gdx.files.internal("Background/logoImage.png"));
     }
 
     public void blinkingEffect(float lowerLimit, BitmapFont font,float increaseValue,float decreaseValue)
@@ -37,6 +40,11 @@ public class MainMenuScreen implements Screen {
        blinkingEffect(0.3f,game.fontRegular,0.04f,0.03f);
 
         game.batch.begin();
+
+        float logoY = (game.SCREEN_HEIGHT - logoImage.getHeight()) / 2 + 50;
+        float logoWidth = ((game.SCREEN_WIDTH +150)/ 2)  ;
+        game.batch.draw(logoImage, (game.SCREEN_WIDTH - logoWidth) / 2, logoY - 5, logoWidth, logoImage.getHeight());
+
         GlyphLayout g1 = new GlyphLayout(game.fontRegular,welcome);
         float textX = (game.SCREEN_WIDTH - g1.width)/2;
         float textY = ((game.SCREEN_HEIGHT - g1.height)/2) +50;
@@ -46,6 +54,7 @@ public class MainMenuScreen implements Screen {
         textX = (game.SCREEN_WIDTH - g1.width)/2;
         textY = (game.SCREEN_HEIGHT - g1.height)/2;
         game.fontRegular.draw(game.batch,g1,textX,textY);
+
 
         game.batch.end();
 
@@ -84,6 +93,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
+        logoImage.dispose();
 
     }
 }
