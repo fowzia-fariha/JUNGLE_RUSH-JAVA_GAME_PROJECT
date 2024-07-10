85a07317a4b33fc00561b498475f13af991559bd
fowzia-fariha <farihashaj75@gmail.com>
2024-07-10 14:32:40 +0600

    Setting up the elements for mainMenuScreen

--- core/src/com/junglerush/MainMenuScreen.java
+++ core/src/com/junglerush/MainMenuScreen.java
@@ -3,6 +3,7 @@ package com.junglerush;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.math.MathUtils;
@@ -11,11 +12,19 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MainMenuScreen implements Screen {
 
     private final JungleRush game;
+    private final Texture logoImage;
+    private final Texture tigImage;
+    private final Texture lioImage;
+    private final Texture deerImage;
     private float opacityControl = 0.3f;
     private boolean increase = true;
     public MainMenuScreen(JungleRush game)
     {
         this.game = game;
+        this.logoImage = new Texture(Gdx.files.internal("Background/logoImage.png"));
+        this.tigImage = new Texture(Gdx.files.internal("Background/tigf.png"));
+        this.lioImage = new Texture(Gdx.files.internal("Background/lionf.png"));
+        this.deerImage = new Texture(Gdx.files.internal("Background/deer.png"));
     }
 
     public void blinkingEffect(float lowerLimit, BitmapFont font,float increaseValue,float decreaseValue)
@@ -37,6 +46,21 @@ public class MainMenuScreen implements Screen {
        blinkingEffect(0.3f,game.fontRegular,0.04f,0.03f);
 
         game.batch.begin();
+
+        float logoY = (game.SCREEN_HEIGHT - logoImage.getHeight()) / 2 + 50;
+        float logoWidth = ((game.SCREEN_WIDTH +150)/ 2)  ;
+        game.batch.draw(logoImage, (game.SCREEN_WIDTH - logoWidth) / 2, logoY - 5, logoWidth, logoImage.getHeight());
+
+        // Scaling factors for the images
+        float scaleTig = 1.4f;
+        float scaleLio = 0.75f;
+        float scaleDeer = 1.3f;
+
+        //  tig, lio, and deer images
+        game.batch.draw(tigImage, 500, -30, tigImage.getWidth() * scaleTig, tigImage.getHeight() * scaleTig);
+        game.batch.draw(lioImage, 1000, 0, lioImage.getWidth() * scaleLio, lioImage.getHeight() * scaleLio);
+        game.batch.draw(deerImage, -200, -100, deerImage.getWidth() * scaleDeer, deerImage.getHeight() * scaleDeer);
+
         GlyphLayout g1 = new GlyphLayout(game.fontRegular,welcome);
         float textX = (game.SCREEN_WIDTH - g1.width)/2;
         float textY = ((game.SCREEN_HEIGHT - g1.height)/2) +50;
@@ -84,6 +108,10 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
+        logoImage.dispose();
+        tigImage.dispose();
+        lioImage.dispose();
+        deerImage.dispose();
 
     }
 }
